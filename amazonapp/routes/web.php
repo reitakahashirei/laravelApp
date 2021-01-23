cd84966716dff676da5063ec66d4204693f14300
Rei Takahashi <rei.takahashi1211@gmail.com>
2021-01-24 04:01:27 +0900

    商品登録機能

--- amazonapp/routes/web.php
+++ amazonapp/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//CRUDにまつわるルートを一括で用意(第一引数：ベースURL、第二引数：コントローラー名)
+//laravel8から以下の記法
+//Route::resource('products', ProductController::class);
+
+Route::get('products', [ProductController::class, 'index'])->name('products.index');
+
+//index.blade.phpでroute(products.show)としているから、name()を使う必要あり
+Route::get('products/show/{id}', [ProductController::class, 'show'])->name('products.show');
+
+Route::get('products/edit/{id}', [ProductController::class, 'edit'])->name('products.edit');
+
+Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
+
+Route::post('products/store', [ProductController::class, 'store']);
+
+Route::post('products/update/{id}', [ProductController::class, 'update']);
+
+Route::post('products/destroy/{id}', [ProductController::class, 'destroy']);
